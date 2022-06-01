0830c63d677527019dbf413679ad6fa7f97529ce
peb4010 <dmdqld24@gmail.com>
2022-06-01 23:45:12 +0900

    fix header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -7,81 +7,84 @@ import CloseModal from "../modal/CloseModal";
 import "../modal/modal.css";
 
 const HeaderWrapper = styled.div`
-  position: sticky;
-  top: 0;
-  z-index: 100;
-  background-color: #fff;
-  display: flex;
-  align-items: center;
-  width: 100%;
-  height: 56px;
+	background-color: #fff;
+	display: flex;
+	align-items: center;
+	width: 100%;
+	height: 56px;
 
-  .close {
-    width: 38px;
-    height: 38px;
-    margin-left: 8px;
-  }
+	.close {
+		width: 38px;
+		height: 38px;
+		margin-left: 8px;
+	}
 
-  .beta {
-    width: 33px;
-    height: 18px;
-    margin-left: 4px;
-  }
+	.beta {
+		width: 33px;
+		height: 18px;
+		margin-left: 4px;
+	}
 
-  img {
-    cursor: pointer;
-  }
+	img {
+		cursor: pointer;
+	}
 
-  @media screen and (min-width: 750px) {
-    .close {
-      display: none;
-    }
+	@media screen and (min-width: 750px) {
+		.close {
+			display: none;
+		}
 
-    .header_title {
-      margin-left: 32px;
-    }
-  }
+		.header_title {
+			margin-left: 32px;
+		}
+	}
+
+	@media screen and (max-width: 749px) {
+		position: sticky;
+		top: 0;
+		z-index: 100;
+	}
 `;
 
 function Header({
-  step,
-  data,
-  page,
+	step,
+	data,
+	page,
 }: {
-  step?: number;
-  data?: any;
-  page: string;
+	step?: number;
+	data?: any;
+	page: string;
 }) {
-  const [closeModal, setCloseModal] = useState(false);
+	const [closeModal, setCloseModal] = useState(false);
 
-  return (
-    <>
-      <Modal
-        isOpen={closeModal}
-        onRequestClose={() => setCloseModal(!closeModal)}
-        overlayClassName="overlay"
-        className="close_modal"
-        ariaHideApp={false}
-      >
-        <CloseModal
-          setCloseModal={setCloseModal}
-          data={data}
-          mainTxt="홈서비스 신청을 취소하시겠습니까?"
-        />
-      </Modal>
-      <HeaderWrapper>
-        <img
-          className="close"
-          src="./img/icon_header_cancel_circle.svg"
-          alt="닫기버튼"
-          onClick={() => setCloseModal(true)}
-        />
-        <RegistTitle className="header_title">차란차 홈서비스 신청</RegistTitle>
-        <img className="beta" src="./img/icon_beta_black.svg" alt="베타" />
-        {page === "1" && <Progress step={step} />}
-      </HeaderWrapper>
-    </>
-  );
+	return (
+		<>
+			<Modal
+				isOpen={closeModal}
+				onRequestClose={() => setCloseModal(!closeModal)}
+				overlayClassName="overlay"
+				className="close_modal"
+				ariaHideApp={false}
+			>
+				<CloseModal
+					setCloseModal={setCloseModal}
+					data={data}
+					mainTxt="홈서비스 신청을 취소하시겠습니까?"
+				/>
+			</Modal>
+			<HeaderWrapper>
+				<img
+					className="close"
+					src="./img/icon_header_cancel_circle.svg"
+					alt="닫기버튼"
+					onClick={() => setCloseModal(true)}
+				/>
+				<RegistTitle className="header_title">차란차 홈서비스 신청</RegistTitle>
+				<img className="beta" src="./img/icon_beta_black.svg" alt="베타" />
+				{page === "1" && <Progress step={step} />}
+			</HeaderWrapper>
+		</>
+	);
 }
 
 export default Header;
