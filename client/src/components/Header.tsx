b6aacbb20dbbff5c26cff396e3eee0b8f486c727
eunbin park <eunbinpark@eunbinui-MacBookPro.local>
2022-06-02 16:01:49 +0900

    d

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import Progress from "./regist/Progress";
 import { RegistTitle } from "../styles/recycle";
@@ -37,6 +37,7 @@ const HeaderWrapper = styled.div`
     }
 
     .header_title {
+      font-family: "Poppins";
       margin-left: 32px;
     }
   }
