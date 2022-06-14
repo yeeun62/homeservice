790c2d322331b07d019ead5288027716bc1a17f1
yeeun62 <byebye62@naver.com>
2022-06-14 09:59:21 +0900

    entertab

--- client/src/components/regist/Step3_1.tsx
+++ client/src/components/regist/Step3_1.tsx
@@ -67,17 +67,15 @@ function Step3_1({ setActivate, setStorageData, storageData }: ActiveProps) {
     if (e.key === "Enter") {
       if (e.target.attributes[0].value === "2") {
         setPostCodeOpen(true);
+        let addressinput = document.getElementsByName("4");
+        if (addressinput.length) return addressinput[0].focus();
       }
-      let nextEl = document.querySelectorAll("input");
-      if (nextEl.length) {
-        return nextEl.forEach((input) => {
-          if (
-            input.tabIndex === Number(e.target.attributes[0].value) + 1 ||
-            (e.target.attributes[0].value === "2" && input.tabIndex === 4)
-          ) {
-            return input.focus();
-          }
-        });
+      if (e.target.attributes[0].value < 4) {
+        document
+          .getElementsByName(
+            (Number(e.target.attributes[0].value) + 1).toString()
+          )[0]
+          .focus();
       }
     }
   }
@@ -137,7 +135,7 @@ function Step3_1({ setActivate, setStorageData, storageData }: ActiveProps) {
           </div>
           <div className="input_div">
             <input
-              tabIndex={1}
+              name="1"
               type="text"
               placeholder="실명을 입력해주세요"
               value={step3.nominee_name}
@@ -156,7 +154,7 @@ function Step3_1({ setActivate, setStorageData, storageData }: ActiveProps) {
           <p>휴대전화 번호</p>
           <div className="input_div">
             <input
-              tabIndex={2}
+              name="2"
               placeholder="숫자만 입력해주세요"
               type="tel"
               pattern="\d*"
@@ -195,7 +193,7 @@ function Step3_1({ setActivate, setStorageData, storageData }: ActiveProps) {
           <div className="flex_form" onClick={() => setPostCodeOpen(true)}>
             <div className="input_div">
               <input
-                tabIndex={3}
+                name="3"
                 type="text"
                 placeholder="주소를 검색해주세요"
                 readOnly
@@ -213,7 +211,7 @@ function Step3_1({ setActivate, setStorageData, storageData }: ActiveProps) {
           <div style={{ position: "relative", marginTop: "12px" }}>
             <div className="input_div">
               <input
-                tabIndex={4}
+                name="4"
                 className="input_margin_top"
                 type="text"
                 value={step3.address.nominee_address}
